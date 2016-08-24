32dbbfb50a875632feb6429e1f43db086e85f357
Carlton Duffett <cjduffett@comcast.net>
2016-08-24 11:21:26 -0400

    Updated county and cousub setup
    Updated data for county and cousub sourced from tiger_cb14_500k. All of
    our synthetic stats will reference the tiger.county and tiger.cousub
    tables that we load this data into.

--- postgres/pgsetup.sh
+++ postgres/pgsetup.sh
@@ -22,7 +22,7 @@ EOF
 
 # Create the new schema and tables
 psql -d fhir_test <<EOF
--- Create the database and adds PostGIS extensions
+-- Add the PostGIS database extensions
 CREATE EXTENSION postgis;
 CREATE EXTENSION fuzzystrmatch;
 CREATE EXTENSION postgis_tiger_geocoder;
@@ -32,53 +32,67 @@ CREATE USER fhir_test WITH PASSWORD 'fhir_test';
 
 -- Create schema
 CREATE SCHEMA synth_ma AUTHORIZATION fhir_test;
--- NOTE: The "tiger" schema is automatically created by PostGIS
+-- NOTE: The "tiger" schema is automatically created by PostGIS.
 GRANT USAGE ON SCHEMA tiger TO public;
+GRANT SELECT ON TABLE tiger.county TO public;
 GRANT SELECT ON TABLE tiger.cousub TO public;
 
--- Synthetic county statistics
-CREATE TABLE synth_ma.synth_county_stats (
-    ct_name character varying(100),
-    ct_fips character varying(3) NOT NULL,
-    sq_mi double precision,
+-- Create disease table
+CREATE SEQUENCE disease_id_seq;
+CREATE TABLE synth_ma.synth_disease (
+    diseasefp smallint NOT NULL DEFAULT nextval('disease_id_seq'),
+    stat_name character varying(100),
+    condition_name character varying(100),
+    code_icd9 character varying(6),
+    code_icd10 character varying(6),
+    code_snomed character varying(8)
+)
+
+WITH (
+    OIDS=FALSE
+);
+ALTER SEQUENCE disease_id_seq OWNER TO fhir_test;
+ALTER TABLE synth_ma.synth_disease OWNER TO fhir_test;
+
+-- Create county fact table
+CREATE TABLE synth_ma.synth_county_facts (
+    cntyidfp character varying(5) NOT NULL,
+    diseasefp smallint NOT NULL,
     pop numeric,
     pop_male numeric,
-    pop_female numeric,
-    pop_sm double precision,
-    ct_poly public.geometry(MultiPolygon,4269),
-    ct_pnt public.geometry
+    pop_female numeric
+)
+WITH (
+    OIDS=FALSE
 );
+ALTER TABLE synth_ma.synth_county_facts OWNER TO fhir_test;
 
-ALTER TABLE synth_ma.synth_county_stats OWNER TO fhir_test;
-
--- Synthetic county subdivison statistics
-CREATE TABLE synth_ma.synth_cousub_stats (
-    ct_name character varying(100),
-    ct_fips character varying(3) NOT NULL,
-    cs_name character varying(100),
-    cs_fips character varying(5) NOT NULL,
-    sq_mi double precision,
+-- Create county subdivision fact table
+CREATE TABLE synth_ma.synth_cousub_facts (
+    cosbid character varying(10) NOT NULL,
+    diseasefp smallint NOT NULL,
     pop numeric,
     pop_male numeric,
-    pop_female numeric,
-    pop_sm double precision,
-    cs_poly public.geometry(MultiPolygon,4269),
-    cs_pnt public.geometry
+    pop_female numeric
+)
+WITH (
+    OIDS=FALSE
 );
-
-ALTER TABLE synth_ma.synth_cousub_stats OWNER TO fhir_test;
+ALTER TABLE synth_ma.synth_cousub_facts OWNER TO fhir_test;
 EOF
 
-# Copy data into the statistics tables
-cat $PWD/data/synth_county.csv | psql -d fhir_test -c "\COPY synth_ma.synth_county_stats FROM STDIN (DELIMITER ',', QUOTE '\"', FORMAT CSV)"
-cat $PWD/data/synth_cousub.csv | psql -d fhir_test -c "\COPY synth_ma.synth_cousub_stats FROM STDIN (DELIMITER ',', QUOTE '\"', FORMAT CSV)"
-cat $PWD/data/cousub.csv | psql -d fhir_test -c "\COPY tiger.cousub FROM STDIN (DELIMITER ',', QUOTE '\"', FORMAT CSV)"
+# County Tiger Data
+# From: tiger_cb14_500k.county
+# Query:
+# SELECT gid, statefp, countyfp, countyns, statefp || countyfp as cntyidfp, name, lsad, aland, awater, the_geom
+# FROM tiger_cb14_500k.county WHERE statefp = '25';
+cat $PWD/data/county.csv | psql -d fhir_test -c "\COPY tiger.county (gid, statefp, countyfp, countyns, cntyidfp, name, lsad, aland, awater, the_geom) FROM STDIN (DELIMITER ',', QUOTE '\"', HEADER TRUE, FORMAT CSV)"
 
-# cleanup after database creation and population
-psql -d fhir_test <<EOF
-VACUUM ANALYZE synth_ma.synth_county_stats;
-VACUUM ANALYZE synth_ma.synth_cousub_stats;
-VACUUM ANALYZE tiger.cousub;
-EOF
+# Cousub Tiger Data
+# From: tiger_cb14_500k.cousub
+# Query:
+# SELECT gid, statefp, countyfp, cousubfp, cousubns, statefp || countyfp || cousubfp as cosbidfp, name, lsad, aland, awater, the_geom
+#  FROM tiger_cb14_500k.cousub WHERE statefp = '25';
+cat $PWD/data/cousub.csv | psql -d fhir_test -c "\COPY tiger.cousub (gid, statefp, countyfp, cousubfp, cousubns, cosbidfp, name, lsad, aland, awater, the_geom) FROM STDIN (DELIMITER ',', QUOTE '\"', HEADER TRUE, FORMAT CSV)"
 
 echo "fhir_test database created"
