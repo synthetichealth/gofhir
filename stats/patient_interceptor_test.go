728b4b5aff497951856f917960a020bb347c15d7
Carlton Duffett <cjduffett@comcast.net>
2016-08-29 03:21:08 -0400

    Added interceptor stats tests
    Still need to do facts tests

--- stats/patient_interceptor_test.go
+++ stats/patient_interceptor_test.go
@@ -1,339 +1,171 @@
 package stats
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
+	"testing"
 
-	"github.com/intervention-engine/fhir/models"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	. "gopkg.in/check.v1"
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/suite"
 )
 
-type InterceptorTestSuite struct {
-	DB  *gorm.DB
-	DAL *StatsDataAccess
+const (
+	BedfordCountyfp = "017"
+	BedfordCousubfp = "04615"
+)
+
+type PatientInterceptorTestSuite struct {
+	suite.Suite
+	db *sql.DB
+	da StatsDataAccess
 }
 
-var _ = Suite(&InterceptorTestSuite{})
+type UnkownResource struct{}
 
-func (s *InterceptorTestSuite) SetUpSuite(c *C) {
-
-	// Setup and connect to the test Postgres database. We test using the fhir_test
-	// database. A script to setup this database is available in the postgres/ folder.
-
-	// configure the GORM Postgres driver and database connection
-	db, err := gorm.Open("postgres", "postgres://fhir_test:fhir_test@localhost/fhir_test?sslmode=disable")
-	db.SingularTable(true) // disable table name pluralization globally
+func (s *PatientInterceptorTestSuite) SetupSuite() {
+	// open a postgres database connection to the test database
+	db, err := sql.Open("postgres", "postgres://fhir_test:fhir_test@localhost/fhir_test?sslmode=disable")
 
 	if err != nil {
-		log.Println("Make sure you run pgsetup.sh to create the fhir_test database before testing.")
+		log.Println("Before testing please setup the fhir_test database using pgsetup.sh")
 		log.Fatal(err)
 	}
 
 	// ping the db to ensure we connected successfully
-	if err := db.DB().Ping(); err != nil {
+	if err := db.Ping(); err != nil {
 		log.Fatal(err)
 	}
-	s.DB = db
-	s.DAL = NewPgStatsDataAccess(db)
+	s.db = db
+
+	// create new data access layer for testing
+	s.da = NewPgStatsDataAccess(db)
 }
 
-func (s *InterceptorTestSuite) TearDownSuite(c *C) {
-
-	s.DB.Close()
+func (s *PatientInterceptorTestSuite) TearDownSuite() {
+	// close the db connection after testing is finished
+	s.db.Close()
 }
 
-// TestPatientStatsCreateInterceptor tests the PatientStatsCreateInterceptor's ability to update
-// patient statistics after a new patient is added to the database
-func (s *InterceptorTestSuite) TestPatientStatsCreateInterceptor(c *C) {
-
-	var patient *models.Patient
-	var county County
-	var cousub Subdivision
-	var countyStat, ucountyStat SyntheticCountyStat
-	var cousubStat, ucousubStat SyntheticSubdivisionStat
-
-	createInterceptor := &PatientStatsCreateInterceptor{
-		DataAccess: s.DAL,
-	}
-
-	patient = &models.Patient{
-		Gender: "male",
-		Address: []models.Address{
-			models.Address{
-				City:       "Boston",
-				State:      "MA",
-				PostalCode: "02215",
-			},
-		},
-	}
-
-	// Get existing stats
-	cousub = s.DAL.Subdivisions.GetSubdivisionByName(patient.Address[0].City)
-	county = s.DAL.Counties.GetCountyById(cousub.CountyFp)
-	countyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	cousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
-
-	// 1. Test a CreateMale operation
-	// ------------------------------------------------------------------------
-	createInterceptor.After(patient)
-	ucountyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	ucousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
-	assertCountyStatChanged(c, countyStat, ucountyStat, 1, 0)
-	assertSubdivisionStatChanged(c, cousubStat, ucousubStat, 1, 0)
-
-	// 2. Test a CreateFemale operation
-	// ------------------------------------------------------------------------
-	patient.Gender = "female"
-	countyStat = ucountyStat
-	cousubStat = ucousubStat
-
-	createInterceptor.After(patient)
-	ucountyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	ucousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
-	assertCountyStatChanged(c, countyStat, ucountyStat, 0, 1)
-	assertSubdivisionStatChanged(c, cousubStat, ucousubStat, 0, 1)
-
+func TestPatientInterceptors(t *testing.T) {
+	// bind test suite to go test
+	suite.Run(t, new(PatientInterceptorTestSuite))
 }
 
-// TestPatientStatsUpdateInterceptor tests the PatientStatsUpdateInterceptor's ability to update
-// patient statistics after a new patient is added to the database
-func (s *InterceptorTestSuite) TestPatientStatsUpdateInterceptor(c *C) {
+func (s *PatientInterceptorTestSuite) TestPatientCreateInterceptor() {
 
-	var patient, updatedPatient *models.Patient
-	var county, county2 County
-	var cousub, cousub2 Subdivision
-	var countyStat, ucountyStat, countyStat2, ucountyStat2 SyntheticCountyStat
-	var cousubStat, ucousubStat, cousubStat2, ucousubStat2 SyntheticSubdivisionStat
+	var countyStats, updatedCountyStats, cousubStats, updatedCousubStats Stats
 
-	updateInterceptor := &PatientStatsUpdateInterceptor{
-		DataAccess: s.DAL,
-	}
+	// Get initial stats for comparison
+	countyStats, _ = s.getCountyStats(BostonCountyfp)
+	cousubStats, _ = s.getSubdivisionStats(BostonCousubfp)
 
-	patient = &models.Patient{
-		Gender: "male",
-		Address: []models.Address{
-			models.Address{
-				City:       "Boston",
-				State:      "MA",
-				PostalCode: "02215",
-			},
-		},
-	}
+	// Add a patient
+	patient := createPatient("Boston", "male")
+	createInterceptor := &PatientStatsCreateInterceptor{DataAccess: s.da}
+	s.NotPanics(func() { createInterceptor.After(patient) }, "Should not panic for valid patient")
 
-	updatedPatient = &models.Patient{
-		Gender: "male",
-		Address: []models.Address{
-			models.Address{
-				City:       "Bedford",
-				State:      "MA",
-				PostalCode: "01730",
-			},
-		},
-	}
+	// Check that the relevant statistics updated
+	updatedCountyStats, _ = s.getCountyStats(BostonCountyfp)
+	updatedCousubStats, _ = s.getSubdivisionStats(BostonCousubfp)
+	s.assertCountyStatsChanged(countyStats, updatedCountyStats, 1, 0)
+	s.assertSubdivisionStatsChanged(cousubStats, updatedCousubStats, 1, 0)
 
-	// Get existing stats
-	cousub = s.DAL.Subdivisions.GetSubdivisionByName(patient.Address[0].City)
-	county = s.DAL.Counties.GetCountyById(cousub.CountyFp)
-	countyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	cousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
+	// Check handling of non-patient resources (fails silently)
+	s.NotPanics(func() { createInterceptor.After(UnkownResource{}) }, "Should not panic for non-patient resource")
+}
 
-	// 3. Test a UpdateMale operation with no relevant changes
-	// ------------------------------------------------------------------------
-	updateInterceptor.Before(patient)
-	updateInterceptor.After(patient)
+func (s *PatientInterceptorTestSuite) TestPatientUpdateInterceptor() {
 
-	ucountyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	ucousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
-	assertCountyStatChanged(c, countyStat, ucountyStat, 0, 0)
-	assertSubdivisionStatChanged(c, cousubStat, ucousubStat, 0, 0)
+	var countyStats, updatedCountyStats, cousubStats, updatedCousubStats Stats
+	var countyStats2, updatedCountyStats2, cousubStats2, updatedCousubStats2 Stats
 
-	// 4. Test a UpdateFemale operation with no relevant changes
-	// ------------------------------------------------------------------------
-	patient.Gender = "female"
-	countyStat = ucountyStat
-	cousubStat = ucousubStat
+	// Get initial stats for comparison
+	countyStats, _ = s.getCountyStats(BostonCountyfp)
+	cousubStats, _ = s.getSubdivisionStats(BostonCousubfp)
 
-	updateInterceptor.Before(patient)
-	updateInterceptor.After(patient)
+	patient := createPatient("Boston", "female")
+	updateInterceptor := &PatientStatsUpdateInterceptor{DataAccess: s.da}
+	s.NotPanics(func() { updateInterceptor.Before(patient) }, "Should not panic for valid patient")
+	s.NotPanics(func() { updateInterceptor.After(patient) }, "Should not panic for valid patient")
 
-	ucountyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	ucousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
-	assertCountyStatChanged(c, countyStat, ucountyStat, 0, 0)
-	assertSubdivisionStatChanged(c, cousubStat, ucousubStat, 0, 0)
+	// Check that the statistics didn't change (since the patient's address didnt change)
+	updatedCountyStats, _ = s.getCountyStats(BostonCountyfp)
+	updatedCousubStats, _ = s.getSubdivisionStats(BostonCousubfp)
+	s.assertCountyStatsChanged(countyStats, updatedCountyStats, 0, 0)
+	s.assertSubdivisionStatsChanged(cousubStats, updatedCousubStats, 0, 0)
 
-	// 5. Test a UpdateFemale operation with relevant statistic changes
-	// ------------------------------------------------------------------------
-	updatedPatient.Gender = "female"
-	countyStat = ucountyStat
-	cousubStat = ucousubStat
+	// Test a change of address
+	countyStats, _ = s.getCountyStats(BostonCountyfp)
+	cousubStats, _ = s.getSubdivisionStats(BostonCousubfp)
+	countyStats2, _ = s.getCountyStats(BedfordCountyfp)
+	cousubStats2, _ = s.getSubdivisionStats(BedfordCousubfp)
 
-	// The patient's City will change from Boston to Bedford, so we need to get Bedford's stats too
-	cousub2 = s.DAL.Subdivisions.GetSubdivisionByName(updatedPatient.Address[0].City)
-	county2 = s.DAL.Counties.GetCountyById(cousub2.CountyFp)
-	countyStat2 = s.DAL.SyntheticCountyStats.GetStatByCounty(county2)
-	cousubStat2 = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub2)
-
+	updatedPatient := createPatient("Bedford", "female")
 	updateInterceptor.Before(patient)
 	updateInterceptor.After(updatedPatient)
 
-	ucountyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	ucousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
-	ucountyStat2 = s.DAL.SyntheticCountyStats.GetStatByCounty(county2)
-	ucousubStat2 = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub2)
+	// Check that Boston's statistics changed
+	updatedCountyStats, _ = s.getCountyStats(BostonCountyfp)
+	updatedCousubStats, _ = s.getSubdivisionStats(BostonCousubfp)
+	s.assertCountyStatsChanged(countyStats, updatedCountyStats, 0, -1)
+	s.assertSubdivisionStatsChanged(cousubStats, updatedCousubStats, 0, -1)
 
-	// Boston stats should have decremented
-	assertCountyStatChanged(c, countyStat, ucountyStat, 0, -1)
-	assertSubdivisionStatChanged(c, cousubStat, ucousubStat, 0, -1)
+	// Check that Bedford's statistics changed
+	updatedCountyStats2, _ = s.getCountyStats(BedfordCountyfp)
+	updatedCousubStats2, _ = s.getSubdivisionStats(BedfordCousubfp)
+	s.assertCountyStatsChanged(countyStats2, updatedCountyStats2, 0, 1)
+	s.assertSubdivisionStatsChanged(cousubStats2, updatedCousubStats2, 0, 1)
 
-	// Bedford stats should have incremented
-	assertCountyStatChanged(c, countyStat2, ucountyStat2, 0, 1)
-	assertSubdivisionStatChanged(c, cousubStat2, ucousubStat2, 0, 1)
-
-	// 6. Test a UpdateMale operation with relevant statistic changes
-	// ------------------------------------------------------------------------
-	patient.Gender = "male"
-	updatedPatient.Gender = "male"
-	countyStat = ucountyStat
-	cousubStat = ucousubStat
-	countyStat2 = ucountyStat2
-	cousubStat2 = ucousubStat2
-
-	updateInterceptor.Before(patient)
-	updateInterceptor.After(updatedPatient)
-
-	ucountyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	ucousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
-	ucountyStat2 = s.DAL.SyntheticCountyStats.GetStatByCounty(county2)
-	ucousubStat2 = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub2)
-
-	// Boston stats should have decremented
-	assertCountyStatChanged(c, countyStat, ucountyStat, -1, 0)
-	assertSubdivisionStatChanged(c, cousubStat, ucousubStat, -1, 0)
-
-	// Bedford stats should have incremented
-	assertCountyStatChanged(c, countyStat2, ucountyStat2, 1, 0)
-	assertSubdivisionStatChanged(c, cousubStat2, ucousubStat2, 1, 0)
+	// Check handling of non-patient resources (fails silently)
+	s.NotPanics(func() { updateInterceptor.Before(UnkownResource{}) }, "Should not panic for non-patient resource")
+	s.NotPanics(func() { updateInterceptor.After(UnkownResource{}) }, "Should not panic for non-patient resource")
 }
 
-// TestPatientStatsDeleteInterceptor tests the PatientStatsDeleteInterceptor's ability to update
-// patient statistics after a new patient is added to the database
-func (s *InterceptorTestSuite) TestPatientStatsDeleteInterceptor(c *C) {
+func (s *PatientInterceptorTestSuite) TestPatientDeleteInterceptor() {
 
-	var patient *models.Patient
-	var county County
-	var cousub Subdivision
-	var countyStat, ucountyStat SyntheticCountyStat
-	var cousubStat, ucousubStat SyntheticSubdivisionStat
+	var countyStats, updatedCountyStats, cousubStats, updatedCousubStats Stats
+	patient := createPatient("Bedford", "female")
+	deleteInterceptor := PatientStatsDeleteInterceptor{DataAccess: s.da}
 
-	deleteInterceptor := &PatientStatsDeleteInterceptor{
-		DataAccess: s.DAL,
-	}
+	countyStats, _ = s.getCountyStats(BedfordCountyfp)
+	cousubStats, _ = s.getSubdivisionStats(BedfordCousubfp)
 
-	patient = &models.Patient{
-		Gender: "male",
-		Address: []models.Address{
-			models.Address{
-				City:       "Boston",
-				State:      "MA",
-				PostalCode: "02215",
-			},
-		},
-	}
+	s.NotPanics(func() { deleteInterceptor.After(patient) }, "Should not panic for valid patient")
 
-	// Get existing stats
-	cousub = s.DAL.Subdivisions.GetSubdivisionByName(patient.Address[0].City)
-	county = s.DAL.Counties.GetCountyById(cousub.CountyFp)
-	countyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	cousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
+	// Check that the stats changed
+	updatedCountyStats, _ = s.getCountyStats(BedfordCountyfp)
+	updatedCousubStats, _ = s.getSubdivisionStats(BedfordCousubfp)
+	s.assertCountyStatsChanged(countyStats, updatedCountyStats, 0, -1)
+	s.assertSubdivisionStatsChanged(cousubStats, updatedCousubStats, 0, -1)
 
-	// 7. Test a DeleteMale operation
-	// ------------------------------------------------------------------------
-	deleteInterceptor.After(patient)
-	ucountyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	ucousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
-	assertCountyStatChanged(c, countyStat, ucountyStat, -1, 0)
-	assertSubdivisionStatChanged(c, cousubStat, ucousubStat, -1, 0)
-
-	// 8. Test a DeleteFemale operation
-	// ------------------------------------------------------------------------
-	patient.Gender = "female"
-	countyStat = ucountyStat
-	cousubStat = ucousubStat
-
-	deleteInterceptor.After(patient)
-	ucountyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	ucousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
-	assertCountyStatChanged(c, countyStat, ucountyStat, 0, -1)
-	assertSubdivisionStatChanged(c, cousubStat, ucousubStat, 0, -1)
+	s.NotPanics(func() { deleteInterceptor.After(UnkownResource{}) }, "Should not panic for non-patient resource")
 }
 
-// TestPatientStatsInterceptorErrorHandling tests that the interceptors and underlying
-// data access layers handle errors as expected
-func (s *InterceptorTestSuite) TestPatientStatsInterceptorErrorHandling(c *C) {
+func (s *PatientInterceptorTestSuite) getCountyStats(countyfp string) (stats Stats, err error) {
+	query := "SELECT pop, pop_male, pop_female, pop_sm, sq_mi FROM synth_ma.synth_county_stats WHERE ct_fips = $1"
+	err = s.db.QueryRow(query, countyfp).Scan(&stats.Pop, &stats.PopMale, &stats.PopFemale, &stats.PopPerSqMile, &stats.SqMiles)
+	return
+}
 
-	var countyStat, ucountyStat SyntheticCountyStat
-	var cousubStat, ucousubStat SyntheticSubdivisionStat
+func (s *PatientInterceptorTestSuite) getSubdivisionStats(cousubfp string) (stats Stats, err error) {
+	query := "SELECT pop, pop_male, pop_female, pop_sm, sq_mi FROM synth_ma.synth_cousub_stats WHERE cs_fips = $1"
+	err = s.db.QueryRow(query, cousubfp).Scan(&stats.Pop, &stats.PopMale, &stats.PopFemale, &stats.PopPerSqMile, &stats.SqMiles)
+	return
+}
 
-	patient := &models.Patient{
-		Gender: "foo",
-		Address: []models.Address{
-			models.Address{
-				City:       "Boston",
-				State:      "MA",
-				PostalCode: "02215",
-			},
-		},
-	}
+func (s *PatientInterceptorTestSuite) assertCountyStatsChanged(county, ucounty Stats, maleDelta, femaleDelta int64) {
+	s.Equal(county.Pop+(maleDelta+femaleDelta), ucounty.Pop, fmt.Sprintf("County population should change by %d", maleDelta+femaleDelta))
+	s.Equal(county.PopMale+maleDelta, ucounty.PopMale, fmt.Sprintf("County male population should change by %d", maleDelta))
+	s.Equal(county.PopFemale+femaleDelta, ucounty.PopFemale, fmt.Sprintf("County female population should change by %d", femaleDelta))
+	newPopPerSqMile := float64(county.Pop+(maleDelta+femaleDelta)) / county.SqMiles
+	s.Equal(newPopPerSqMile, ucounty.PopPerSqMile, fmt.Sprintf("County PopPerSqMile should now be %.8f", newPopPerSqMile))
+}
 
-	createInterceptor := &PatientStatsCreateInterceptor{
-		DataAccess: s.DAL,
-	}
-
-	updateInterceptor := &PatientStatsUpdateInterceptor{
-		DataAccess: s.DAL,
-	}
-
-	deleteInterceptor := &PatientStatsDeleteInterceptor{
-		DataAccess: s.DAL,
-	}
-
-	cousub := s.DAL.Subdivisions.GetSubdivisionByName(patient.Address[0].City)
-	county := s.DAL.Counties.GetCountyById(cousub.CountyFp)
-	countyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	cousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
-
-	// 9. Test an invalid gender (fails silently and doesn't modify DB)
-	// ------------------------------------------------------------------------
-	createInterceptor.After(patient)
-	ucountyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	ucousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
-	assertCountyStatChanged(c, countyStat, ucountyStat, 0, 0)
-	assertSubdivisionStatChanged(c, cousubStat, ucousubStat, 0, 0)
-
-	countyStat = ucountyStat
-	cousubStat = ucousubStat
-	updateInterceptor.Before(patient)
-	updateInterceptor.After(patient)
-	ucountyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	ucousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
-	assertCountyStatChanged(c, countyStat, ucountyStat, 0, 0)
-	assertSubdivisionStatChanged(c, cousubStat, ucousubStat, 0, 0)
-
-	countyStat = ucountyStat
-	cousubStat = ucousubStat
-	deleteInterceptor.After(patient)
-	ucountyStat = s.DAL.SyntheticCountyStats.GetStatByCounty(county)
-	ucousubStat = s.DAL.SyntheticSubdivisionStats.GetStatBySubdivision(cousub)
-	assertCountyStatChanged(c, countyStat, ucountyStat, 0, 0)
-	assertSubdivisionStatChanged(c, cousubStat, ucousubStat, 0, 0)
-
-	// 10. Test a city that does not exist (fails silently)
-	// ------------------------------------------------------------------------
-	patient.Gender = "male"
-	patient.Address[0].City = "foo"
-
-	createInterceptor.After(patient)
-	updateInterceptor.After(patient)
-	deleteInterceptor.After(patient)
+func (s *PatientInterceptorTestSuite) assertSubdivisionStatsChanged(cousub, ucousub Stats, maleDelta, femaleDelta int64) {
+	s.Equal(cousub.Pop+(maleDelta+femaleDelta), ucousub.Pop, fmt.Sprintf("Subdivision population should change by %d", maleDelta+femaleDelta))
+	s.Equal(cousub.PopMale+maleDelta, ucousub.PopMale, fmt.Sprintf("Subdivision male population should change by %d", maleDelta))
+	s.Equal(cousub.PopFemale+femaleDelta, ucousub.PopFemale, fmt.Sprintf("Subivision female population should change by %d", femaleDelta))
+	newPopPerSqMile := float64(cousub.Pop+(maleDelta+femaleDelta)) / cousub.SqMiles
+	s.Equal(newPopPerSqMile, ucousub.PopPerSqMile, fmt.Sprintf("Subdivision PopPerSqMile should now be %.8f", newPopPerSqMile))
 }
