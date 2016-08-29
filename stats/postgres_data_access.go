946d522545664f2c415458bcf828f335ed4b3c27
Carlton Duffett <cjduffett@comcast.net>
2016-08-29 13:46:27 -0400

    Minor Improvements
    Add/Remove methods are now a little more DRY.
    Added additional error handling check for patient address being nil.
    Added additional error check for Snomed codes.

--- stats/postgres_data_access.go
+++ stats/postgres_data_access.go
@@ -14,8 +14,10 @@ const SnomedCodeSystem = "http://snomed.info/sct"
 const ICD9CodeSystem = "http://www.icd9data.com/"
 const ICD10CodeSystem = "http://www.icd10data.com/"
 
-var InvalidGenderError = errors.New("Invalid gender")
-var InvalidUpdateOperationError = errors.New("Invalid update operation")
+var ErrInvalidGender = errors.New("Invalid gender")
+var ErrInvalidUpdateOperation = errors.New("Invalid update operation")
+var ErrNoAddress = errors.New("No address found")
+var ErrNoSnomedCode = errors.New("No snomed code found")
 
 // StatsDataAccess is the top level interface for interacting with Synthetic Mass statistics
 type StatsDataAccess interface {
@@ -39,83 +41,39 @@ func NewPgStatsDataAccess(db *sql.DB) *PgStatsDataAccess {
 // based on the given patient's address.
 func (da *PgStatsDataAccess) AddPatientStat(patient *models.Patient) (err error) {
 
-	countyfp, cousubfp, err := da.identifyCountyAndSubdivisionForPatient(patient)
-
-	if err != nil {
-		return
-	}
-
-	if patientGenderIsValid(patient) {
-		err = da.updateStats(countyfp, cousubfp, patient.Gender, "increment")
-
-	} else {
-		err = InvalidGenderError
-	}
-	return
+	return da.updateStats(patient, "increment")
 }
 
 // RemovePatientStat decrements the relevant county and subdivision statistics
 // based on the given patient's address.
 func (da *PgStatsDataAccess) RemovePatientStat(patient *models.Patient) (err error) {
 
-	countyfp, cousubfp, err := da.identifyCountyAndSubdivisionForPatient(patient)
-
-	if err != nil {
-		return
-	}
-
-	if patientGenderIsValid(patient) {
-		err = da.updateStats(countyfp, cousubfp, patient.Gender, "decrement")
-
-	} else {
-		err = InvalidGenderError
-	}
-	return
+	return da.updateStats(patient, "decrement")
 }
 
 // AddConditionStat increments the relevant county and subdivision condition facts
 // based on the given patient's address.
 func (da *PgStatsDataAccess) AddConditionStat(patient *models.Patient, condition *models.Condition) (err error) {
 
-	countyfp, cousubfp, err := da.identifyCountyAndSubdivisionForPatient(patient)
-	diseasefp, err := da.identifyDiseaseForCondition(condition)
-
-	if err != nil {
-		return
-	}
-
-	if patientGenderIsValid(patient) {
-		err = da.updateFacts(countyfp, cousubfp, diseasefp, patient.Gender, "increment")
-
-	} else {
-		err = InvalidGenderError
-	}
-	return
+	return da.updateFacts(patient, condition, "increment")
 }
 
 // RemoveConditionStat decrements the relevant county and subdivision condition facts
 // based on the given patient's address.
 func (da *PgStatsDataAccess) RemoveConditionStat(patient *models.Patient, condition *models.Condition) (err error) {
 
-	countyfp, cousubfp, err := da.identifyCountyAndSubdivisionForPatient(patient)
-	diseasefp, err := da.identifyDiseaseForCondition(condition)
-
-	if err != nil {
-		return
-	}
-
-	if patientGenderIsValid(patient) {
-		err = da.updateFacts(countyfp, cousubfp, diseasefp, patient.Gender, "decrement")
-
-	} else {
-		err = InvalidGenderError
-	}
-	return
+	return da.updateFacts(patient, condition, "decrement")
 }
 
 // identifyCountyAndSubdivisionForPatient returns the countyfp and cousubfp that
 // match the subdivision in the given patient's address.
 func (da *PgStatsDataAccess) identifyCountyAndSubdivisionForPatient(patient *models.Patient) (countyfp, cousubfp string, err error) {
+
+	if len(patient.Address) == 0 || patient.Address[0].City == "" {
+		err = ErrNoAddress
+		return
+	}
+
 	err = da.DB.QueryRow("SELECT countyfp, cousubfp FROM tiger.cousub WHERE name = $1", patient.Address[0].City).Scan(&countyfp, &cousubfp)
 	return
 }
@@ -123,12 +81,28 @@ func (da *PgStatsDataAccess) identifyCountyAndSubdivisionForPatient(patient *mod
 // identifyDiseaseForCondition returns the diseasefp that matches the given
 // condition's SNOMED code, if any.
 func (da *PgStatsDataAccess) identifyDiseaseForCondition(condition *models.Condition) (diseasefp string, err error) {
-	err = da.DB.QueryRow("SELECT diseasefp FROM synth_ma.synth_disease WHERE code_snomed = $1", getSnomedCode(condition)).Scan(&diseasefp)
+	snomedCode, err := getSnomedCode(condition)
+
+	if err != nil || snomedCode == "" {
+		return "", ErrNoSnomedCode
+	}
+	err = da.DB.QueryRow("SELECT diseasefp FROM synth_ma.synth_disease WHERE code_snomed = $1", snomedCode).Scan(&diseasefp)
 	return
 }
 
 // updateStats increments or decrements a row of population counts in the county and subdivision stats tables.
-func (da *PgStatsDataAccess) updateStats(countyfp, cousubfp, gender, op string) (err error) {
+func (da *PgStatsDataAccess) updateStats(patient *models.Patient, op string) (err error) {
+
+	countyfp, cousubfp, err := da.identifyCountyAndSubdivisionForPatient(patient)
+
+	if err != nil {
+		return
+	}
+
+	if !patientGenderIsValid(patient) {
+		err = ErrInvalidGender
+		return
+	}
 
 	var symbol string
 	switch op {
@@ -137,19 +111,31 @@ func (da *PgStatsDataAccess) updateStats(countyfp, cousubfp, gender, op string) 
 	case "decrement":
 		symbol = "-"
 	default:
-		return InvalidUpdateOperationError
+		return ErrInvalidUpdateOperation
 	}
 
 	var ctfp, csfp int
-	countyQuery := fmt.Sprintf("UPDATE synth_ma.synth_county_stats SET pop = pop %s 1, pop_%s = pop_%s %s 1, pop_sm = ((pop %s 1) / sq_mi) WHERE ct_fips = $1 RETURNING ct_fips", symbol, gender, gender, symbol, symbol)
-	cousubQuery := fmt.Sprintf("UPDATE synth_ma.synth_cousub_stats SET pop = pop %s 1, pop_%s = pop_%s %s 1, pop_sm = ((pop %s 1) / sq_mi) WHERE cs_fips = $1 RETURNING cs_fips", symbol, gender, gender, symbol, symbol)
+	countyQuery := fmt.Sprintf("UPDATE synth_ma.synth_county_stats SET pop = pop %s 1, pop_%s = pop_%s %s 1, pop_sm = ((pop %s 1) / sq_mi) WHERE ct_fips = $1 RETURNING ct_fips", symbol, patient.Gender, patient.Gender, symbol, symbol)
+	cousubQuery := fmt.Sprintf("UPDATE synth_ma.synth_cousub_stats SET pop = pop %s 1, pop_%s = pop_%s %s 1, pop_sm = ((pop %s 1) / sq_mi) WHERE cs_fips = $1 RETURNING cs_fips", symbol, patient.Gender, patient.Gender, symbol, symbol)
 	err = da.DB.QueryRow(countyQuery, countyfp).Scan(&ctfp)
 	err = da.DB.QueryRow(cousubQuery, cousubfp).Scan(&csfp)
 	return
 }
 
 // updateFacts increments or decrements a row of population counts in the county and subdivision fact tables.
-func (da *PgStatsDataAccess) updateFacts(countyfp, cousubfp, diseasefp, gender, op string) (err error) {
+func (da *PgStatsDataAccess) updateFacts(patient *models.Patient, condition *models.Condition, op string) (err error) {
+
+	countyfp, cousubfp, err := da.identifyCountyAndSubdivisionForPatient(patient)
+	diseasefp, err := da.identifyDiseaseForCondition(condition)
+
+	if err != nil {
+		return
+	}
+
+	if !patientGenderIsValid(patient) {
+		err = ErrInvalidGender
+		return
+	}
 
 	var symbol string
 	switch op {
@@ -158,12 +144,12 @@ func (da *PgStatsDataAccess) updateFacts(countyfp, cousubfp, diseasefp, gender, 
 	case "decrement":
 		symbol = "-"
 	default:
-		return InvalidUpdateOperationError
+		return ErrInvalidUpdateOperation
 	}
 
 	var ctfp, csfp int
-	countyQuery := fmt.Sprintf("UPDATE synth_ma.synth_county_facts SET pop = pop %s 1, pop_%s = pop_%s %s 1 WHERE countyfp = $1 AND diseasefp = $2 RETURNING countyfp", symbol, gender, gender, symbol)
-	cousubQuery := fmt.Sprintf("UPDATE synth_ma.synth_cousub_facts SET pop = pop %s 1, pop_%s = pop_%s %s 1 WHERE cousubfp = $1 AND diseasefp = $2 RETURNING cousubfp", symbol, gender, gender, symbol)
+	countyQuery := fmt.Sprintf("UPDATE synth_ma.synth_county_facts SET pop = pop %s 1, pop_%s = pop_%s %s 1 WHERE countyfp = $1 AND diseasefp = $2 RETURNING countyfp", symbol, patient.Gender, patient.Gender, symbol)
+	cousubQuery := fmt.Sprintf("UPDATE synth_ma.synth_cousub_facts SET pop = pop %s 1, pop_%s = pop_%s %s 1 WHERE cousubfp = $1 AND diseasefp = $2 RETURNING cousubfp", symbol, patient.Gender, patient.Gender, symbol)
 	err = da.DB.QueryRow(countyQuery, countyfp, diseasefp).Scan(&ctfp)
 	err = da.DB.QueryRow(cousubQuery, cousubfp, diseasefp).Scan(&csfp)
 	return
@@ -175,12 +161,12 @@ func patientGenderIsValid(patient *models.Patient) bool {
 }
 
 // getSnomedCode returns the condition's SNOMED code, if it exists
-func getSnomedCode(condition *models.Condition) string {
+func getSnomedCode(condition *models.Condition) (code string, err error) {
 	codings := condition.Code.Coding
 	for _, coding := range codings {
 		if coding.System == SnomedCodeSystem {
-			return coding.Code
+			return coding.Code, nil
 		}
 	}
-	return ""
+	return "", ErrNoSnomedCode
 }
