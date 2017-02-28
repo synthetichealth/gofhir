8c24cf57b68125eb41be6c4ff8319ebabcdcff68
Carlton Duffett <cjduffett@comcast.net>
2017-02-27 19:29:23 -0500

    Updated command line flags

--- fhir_run.sh
+++ fhir_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /opt/gofhir
-nohup ./gofhir -pgurl postgres://fhir:fhir@localhost/fhir?sslmode=disable > ./gofhir.log &
+nohup ./gofhir -readonly -disable-ci-searches > ./gofhir.log &
