4d380eb3e8fcacbd62387f016bf54c8fdbfe3eb4
Carlton Duffett <cjduffett@comcast.net>
2017-03-02 10:17:11 -0500

    Search and config updates

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"flag"
 	"log"
 	"os"
+	"time"
 
 	mgo "gopkg.in/mgo.v2"
 
-	"github.com/gin-gonic/gin"
 	"github.com/intervention-engine/fhir/server"
 	_ "github.com/synthetichealth/gofhir/synthma"
 )
@@ -15,40 +15,25 @@ import (
 func main() {
 	// set up the commandline flags (-mongo and -pgurl)
 	reqLog := flag.Bool("reqlog", false, "Enables request logging -- do NOT use in production")
-	serverURL := flag.String("server", "", "The full URL for the root of the server")
+	serverURL := flag.String("server", "localhost:3001", "The full URL for the root of the server")
 	dbName := flag.String("dbname", "fhir", "Mongo database name")
 	idxConfigPath := flag.String("idxconfig", "config/indexes.conf", "Path to the indexes config file")
 	mongoHost := flag.String("mongohost", "localhost", "the hostname of the mongo database")
 	readOnly := flag.Bool("readonly", false, "Run the API in read-only mode (no creates, updates, or deletes allowed)")
 	debug := flag.Bool("debug", false, "Enables debug output for the mgo driver")
 	disableCISearches := flag.Bool("disable-ci-searches", false, "Disables case-insensitive searches using regexes")
+	dbTimeout := flag.String("db-timeout", "1m", "Database timeout, for example 45s, 1m, 300ms, etc.")
 
 	flag.Parse()
 
-	// setup the server
-	s := server.NewServer(*mongoHost)
-
 	config := server.DefaultConfig
 
-	if *serverURL != "" {
-		config.ServerURL = *serverURL
-	}
-
-	if *dbName != "" {
-		config.DatabaseName = *dbName
-	}
-
-	if *idxConfigPath != "" {
-		config.IndexConfigPath = *idxConfigPath
-	}
-
-	if *reqLog {
-		s.Engine.Use(server.RequestLoggerHandler)
-	}
-
-	if *readOnly {
-		s.Engine.Use(ReadOnlyHandler)
-	}
+	// If these flags aren't set, the default values are used.
+	config.ServerURL = *serverURL
+	config.DatabaseName = *dbName
+	config.IndexConfigPath = *idxConfigPath
+	config.DatabaseHost = *mongoHost
+	config.ReadOnly = *readOnly
 
 	if *debug {
 		mgo.SetDebug(true)
@@ -57,24 +42,18 @@ func main() {
 		mgo.SetLogger(aLogger)
 	}
 
-	if *disableCISearches {
-		config.EnableCISearches = false
+	config.EnableCISearches = !*disableCISearches
+	duration, err := time.ParseDuration(*dbTimeout)
+	if err == nil {
+		config.DatabaseTimeout = duration
 	}
 
-	s.Run(config)
-}
+	// setup the server
+	s := server.NewServer(config)
 
-// ReadOnlyHandler makes the API read-only and responds to any requests that are not
-// GET, HEAD, or OPTIONS with a 403 Forbidden error.
-func ReadOnlyHandler(c *gin.Context) {
-
-	method := c.Request.Method
-	switch method {
-	// allowed methods:
-	case "GET", "HEAD", "OPTIONS":
-		return
-	// all other methods:
-	default:
-		c.AbortWithStatus(403)
+	if *reqLog {
+		s.Engine.Use(server.RequestLoggerHandler)
 	}
+
+	s.Run()
 }
